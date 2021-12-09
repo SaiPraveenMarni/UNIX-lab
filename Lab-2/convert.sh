a040800462512bbf94cc1d9c857844514a1e74a7
SaiPraveenMarni <63604455+SaiPraveenMarni@users.noreply.github.com>
2021-12-09 21:43:47 +0530

    Update convert.sh

--- Lab-2/convert.sh
+++ Lab-2/convert.sh
@@ -2,4 +2,4 @@
 echo "Enter the temperature in Celcius"
 read celsius
 echo "Temperature in fahrenheit is"
-echo "scale=3; (((9 * $celsius) / 5) + 32)" | bc
+echo "scale=3; (((9 * $celsius) / 5) + 32)" | bc)
